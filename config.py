2b22228e24216f2f9f2ed95f4bd2d7d2e95ee8f9
2333-123 <65855907+2333-123@users.noreply.github.com>
2022-09-16 20:27:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ Blog: https://www.52pojie.cn/?1507943
 Copyright (c) 2022 by ForMemRs, All Rights Reserved.
 '''
 
-COIN_OR_NOT = False
+COIN_OR_NOT = True
 # 是否投币
 # 开启后如果硬币数量小于5默认也会跳过
 
@@ -26,11 +26,11 @@ UID_LIST = ['473837611', '1131457022', '433587902', '2026561407', '50329118']
 # 获取UID的方法见README.md
 # 新华网 人民日报 央视频  王冰冰 英雄联盟赛事
 
-COOKIE_LIST = ['']
+COOKIE_LIST = ['sid=7g9u4fnx; DedeUserID=325636191; DedeUserID__ckMd5=9a80ef2ba1f6f6bb; SESSDATA=411fe845%2C1675512532%2C004ca*81; bili_jct=69777ed52171e36020f130a9cf1783ee; buvid3=7B8DEA2D-F526-0F6E-443A-989D647E7D0338604infoc; i-wanna-go-back=-1; b_ut=5; _uuid=5FAD1176-11F9-DFF4-14E1-22910695DED5257160infoc; buvid4=739BD274-54FA-F1BA-8C2B-A11E9481A69F19603-022012119-RSY0y6c/4STOPLtHpmFe0A%3D%3D; LIVE_BUVID=AUTO6816599606642390; buvid_fp=0a271cab2379f604f3861935cca06769; CURRENT_FNVAL=4048; fingerprint3=3408ac9a7c570f46c1a30ad18b016364; fingerprint=7a0a0df05db8e07847293d178dc1feae; PVID=4']
 # Bilibili的COOKIE获取的方法见README.md
 # 支持多账号，需要多账号请在列表中添加多个COOKIE
 
-PUSH_OR_NOT = False
+PUSH_OR_NOT = True
 # 是否推送消息
-TOKEN = ''
+TOKEN = '4617cfdc6c0342e388d8552a56b340af'
 # PUSH PLUS的TOKEN 官网为 https://www.pushplus.plus
